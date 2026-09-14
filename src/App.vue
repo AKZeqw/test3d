4a299161683e52a934684c9051e89eca7ae7b2d6
AKZeqw <kurniawanagung2005@gmail.com>
2026-09-14 16:02:51 +0700

    init 3d

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+</script>
+
+<template>
+  <div class="app">
+    <TresCanvas clear-color="#1a1a1a">
+      
+      <!-- Camera -->
+      <TresPerspectiveCamera
+        :position="[8, 6, 8]"
+        :look-at="[0, 0, 0]"
+      />
+
+      <!-- Light -->
+      <TresAmbientLight :intensity="1.5" />
+
+      <TresDirectionalLight
+        :position="[5, 10, 5]"
+        :intensity="2"
+      />
+
+      <!-- Lantai -->
+      <TresMesh :rotation="[-Math.PI / 2, 0, 0]">
+        <TresPlaneGeometry :args="[10, 10]" />
+        <TresMeshStandardMaterial color="#555555" />
+      </TresMesh>
+
+      <!-- Meja -->
+      <TresMesh :position="[0, 1, 0]">
+        <TresBoxGeometry :args="[3, 0.2, 2]" />
+        <TresMeshStandardMaterial color="#8b4513" />
+      </TresMesh>
+
+      <!-- Kursi -->
+      <TresMesh :position="[0, 0.5, 2]">
+        <TresBoxGeometry :args="[1, 1, 1]" />
+        <TresMeshStandardMaterial color="#333333" />
+      </TresMesh>
+
+    </TresCanvas>
+  </div>
+</template>
+
+<style>
+.app {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/InvestigationRoom.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { TresCanvas } from '@tresjs/core'
+</script>
+
+<template>
+  <TresCanvas clear-color="#202020">
+
+    <!-- Camera -->
+    <TresPerspectiveCamera
+      :position="[8, 6, 8]"
+      :look-at="[0, 0, 0]"
+    />
+
+    <!-- Lighting -->
+    <TresAmbientLight :intensity="1" />
+
+    <TresDirectionalLight
+      :position="[5, 10, 5]"
+      :intensity="2"
+    />
+
+    <!-- Floor -->
+    <TresMesh :rotation="[-Math.PI / 2, 0, 0]">
+      <TresPlaneGeometry :args="[10, 10]" />
+      <TresMeshStandardMaterial color="#808080" />
+    </TresMesh>
+
+    <!-- Table -->
+    <TresMesh :position="[0, 1, 0]">
+      <TresBoxGeometry :args="[3, 0.2, 2]" />
+      <TresMeshStandardMaterial color="#8B4513" />
+    </TresMesh>
+
+    <!-- Chair -->
+    <TresMesh :position="[0, 0.5, 2]">
+      <TresBoxGeometry :args="[1, 1, 1]" />
+      <TresMeshStandardMaterial color="#444444" />
+    </TresMesh>
+
+  </TresCanvas>
+</template>
